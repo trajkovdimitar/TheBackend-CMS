ba6c4d91741ef91ed30e8cbf94940fd71811d928
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2025-07-06 16:52:07 +0200

    Ensure module assemblies load for discovery

--- TheBackendCmsSolution/Modules/Abstractions/ModuleLoader.cs
+++ TheBackendCmsSolution/Modules/Abstractions/ModuleLoader.cs
@@ -6,10 +6,30 @@ public static class ModuleLoader
 {
     public static IEnumerable<ICmsModule> DiscoverModules()
     {
-        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+
+        // Ensure module assemblies are loaded so they can be discovered even if
+        // no types have been referenced yet.
+        foreach (var path in Directory.EnumerateFiles(AppContext.BaseDirectory, "TheBackendCmsSolution.Modules.*.dll"))
+        {
+            try
+            {
+                var name = AssemblyName.GetAssemblyName(path);
+                if (!assemblies.Any(a => a.GetName().Name == name.Name))
+                {
+                    assemblies.Add(Assembly.Load(name));
+                }
+            }
+            catch
+            {
+                // Ignore load failures so one bad assembly does not prevent startup
+            }
+        }
+
         var moduleTypes = assemblies
             .SelectMany(a => a.GetTypes())
             .Where(t => typeof(ICmsModule).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+
         return moduleTypes.Select(t => (ICmsModule)Activator.CreateInstance(t)!);
     }
 }
