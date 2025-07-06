e25eab93618cd27dec1bdb5d50a17eb2758f8c75
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2025-07-06 22:19:42 +0200

    Add tenants migrations and design-time factory

---
+++ TheBackendCmsSolution/Modules/Tenants/Migrations/20250706190906_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBackendCmsSolution.Modules.Tenants.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Host = table.Column<string>(type: "text", nullable: true),
+                    UrlPrefix = table.Column<string>(type: "text", nullable: true),
+                    ConnectionString = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tenants");
+        }
+    }
+}
