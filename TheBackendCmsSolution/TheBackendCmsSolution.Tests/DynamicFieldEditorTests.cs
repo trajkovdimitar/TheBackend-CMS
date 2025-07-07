5f18c179611c9b1f49a55617d83bc6e7258983d9
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2025-07-07 02:30:33 +0200

    test: add content and component tests

---
+++ TheBackendCmsSolution/TheBackendCmsSolution.Tests/DynamicFieldEditorTests.cs
@@ -0,0 +1,28 @@
+using Bunit;
+using Xunit;
+using TheBackendCmsSolution.Web.Components.FieldEditors;
+
+public class DynamicFieldEditorTests : TestContext
+{
+    [Fact]
+    public void RendersInput_ForString()
+    {
+        var component = RenderComponent<DynamicFieldEditor>(parameters => parameters
+            .Add(p => p.FieldName, "Title")
+            .Add(p => p.FieldType, "string")
+            .Add(p => p.Value, "Hello"));
+
+        Assert.NotNull(component.Find("input"));
+    }
+
+    [Fact]
+    public void RendersTextArea_ForStringArray()
+    {
+        var component = RenderComponent<DynamicFieldEditor>(parameters => parameters
+            .Add(p => p.FieldName, "Tags")
+            .Add(p => p.FieldType, "string[]")
+            .Add(p => p.Value, new[] { "a", "b" }));
+
+        Assert.NotNull(component.Find("textarea"));
+    }
+}
