b6d958852f62cb7452fb64c477fca8d89d74f154
Trajkov Dimitar <45421293+trajkovdimitar@users.noreply.github.com>
2025-07-07 02:34:28 +0200

    Add taxonomy Postgres container

--- TheBackendCmsSolution/TheBackendCmsSolution.AppHost/Program.cs
+++ TheBackendCmsSolution/TheBackendCmsSolution.AppHost/Program.cs
@@ -8,9 +8,13 @@ var contentDb = contentPostgres.AddDatabase("contentdb");
 var tenantsPostgres = builder.AddPostgres("tenantsdbserver").WithPgAdmin();
 var tenantsDb = tenantsPostgres.AddDatabase("tenantsdb");
 
+var taxonomyPostgres = builder.AddPostgres("taxonomydbserver").WithPgAdmin();
+var taxonomyDb = taxonomyPostgres.AddDatabase("taxonomydb");
+
 var apiService = builder.AddProject<Projects.TheBackendCmsSolution_ApiService>("apiservice")
                        .WithReference(contentDb)
-                       .WithReference(tenantsDb);
+                       .WithReference(tenantsDb)
+                       .WithReference(taxonomyDb);
 builder.AddProject<Projects.TheBackendCmsSolution_Web>("webfrontend")
        .WithReference(apiService);
 
